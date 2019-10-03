983dfe60a945a47a18ed517a522e21e844ae207f
roger <roger@prestamosprima.com>
2019-10-03 17:46:17 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,57 @@
 import 'normalize.css';
-import './styles/style.scss';
+import './styles/style.less';
 
 console.log('hello world');
+
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+let clickX = [];
+let clickY = [];
+let clickDrag = [];
+let paint = false;
+
+let addClick = (x, y, dragging) => {
+    clickX.push(x);
+    clickY.push(y);
+    clickDrag.push(dragging);
+    console.log('CLICKS', clickX);
+};
+let redraw = () => {
+    context.strokeStyle = "black";
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+    clickX.forEach((c, index) => {
+            context.beginPath();
+            if (clickDrag[index]) {
+                console.log('drawing');
+                context.moveTo(clickX[index-1], clickY[index-1]);
+            }else{
+                context.moveTo(clickX[index-1], clickY[index]);
+            }
+            context.lineTo(clickX[index], clickY[index]);
+            context.closePath();
+            context.stroke();
+        }
+    );
+}
+canvas.onmousedown = (e) => {
+    console.log('mouseDown', e, e.clientX, e.offsetX);
+    paint = true;
+    addClick(e.clientX - e.offsetX, e.clientY - e.offsetTop);
+    redraw();
+};
+
+canvas.onmousemove = (e) => {
+    if (paint) {
+        addClick(e.clientX - e.offsetX, e.clientY - e.offsetTop);
+        redraw();
+    }
+}
+
+canvas.onmouseup = () => {
+    paint = false;
+};
+
+canvas.onmouseleave = ()=>{
+    paint = false;
+}
