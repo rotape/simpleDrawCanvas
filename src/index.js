f92cee7f0f75a505c5892357750e2d994942c576
roger <roger@prestamosprima.com>
2019-10-04 17:57:58 +0200

    Drawing colors, and thickness

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './styles/style.less';
 class DrawCanvas {
     constructor() {
         this.canvas = document.getElementById('canvas');
+        this.canvas.width = '1024';
+        this.canvas.height = '1024';
         this.context = this.canvas.getContext('2d');
         this.prevPosition = {x: 0, y: 0};
         this.currentPosition = {x: 0, y: 0};
@@ -15,7 +17,7 @@ class DrawCanvas {
             this.isMouseCLicked = true;
             this.updateCurrentPosition(e);
         };
-        this.canvas.onmouseup = ()=>{
+        this.canvas.onmouseup = () => {
             this.isMouseCLicked = false;
         };
         this.canvas.onmousemove = (e) => {
@@ -23,7 +25,10 @@ class DrawCanvas {
                 this.updateCurrentPosition(e);
                 this.draw()
             }
-        }
+        };
+        document.getElementById('clear').addEventListener('click', () => {
+            this.clear()
+        });
     }
 
     updateCurrentPosition(e) {
@@ -32,11 +37,23 @@ class DrawCanvas {
     }
 
     draw() {
+        this.color = document.querySelector('input[name=color]:checked').value;
+        this.lineWidth = document.querySelector('input[name=lineWidth]:checked').value;
+        this.context.strokeStyle = this.color;
+        this.context.lineWidth = this.lineWidth;
         this.context.beginPath();
         this.context.moveTo(this.prevPosition.x, this.prevPosition.y);
         this.context.lineTo(this.currentPosition.x, this.currentPosition.y);
         this.context.stroke();
     }
+
+    clear() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    chooseStrokeWidth(value) {
+        this.context.lineWidth(value);
+    }
 }
 
- new DrawCanvas();
+new DrawCanvas();
