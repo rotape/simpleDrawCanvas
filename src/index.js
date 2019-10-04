0ab8b94d0b057d6d9987e787d6dd2268413e2926
roger <roger@prestamosprima.com>
2019-10-04 13:40:11 +0200

    Drawing, but still missing right scale

--- src/index.js
+++ src/index.js
@@ -1,57 +1,42 @@
 import 'normalize.css';
 import './styles/style.less';
 
-console.log('hello world');
+class DrawCanvas {
+    constructor() {
+        this.canvas = document.getElementById('canvas');
+        this.context = this.canvas.getContext('2d');
+        this.prevPosition = {x: 0, y: 0};
+        this.currentPosition = {x: 0, y: 0};
+        this.init();
+    }
 
-let canvas = document.getElementById('canvas');
-let context = canvas.getContext('2d');
-let clickX = [];
-let clickY = [];
-let clickDrag = [];
-let paint = false;
-
-let addClick = (x, y, dragging) => {
-    clickX.push(x);
-    clickY.push(y);
-    clickDrag.push(dragging);
-    console.log('CLICKS', clickX);
-};
-let redraw = () => {
-    context.strokeStyle = "black";
-    context.lineJoin = "round";
-    context.lineWidth = 5;
-    clickX.forEach((c, index) => {
-            context.beginPath();
-            if (clickDrag[index]) {
-                console.log('drawing');
-                context.moveTo(clickX[index-1], clickY[index-1]);
-            }else{
-                context.moveTo(clickX[index-1], clickY[index]);
+    init() {
+        this.canvas.onmousedown = (e) => {
+            this.isMouseCLicked = true;
+            this.updateCurrentPosition(e);
+        };
+        this.canvas.onmouseup = ()=>{
+            this.isMouseCLicked = false;
+        };
+        this.canvas.onmousemove = (e) => {
+            if (this.isMouseCLicked) {
+                this.updateCurrentPosition(e);
+                this.draw()
             }
-            context.lineTo(clickX[index], clickY[index]);
-            context.closePath();
-            context.stroke();
         }
-    );
-}
-canvas.onmousedown = (e) => {
-    console.log('mouseDown', e, e.clientX, e.offsetX);
-    paint = true;
-    addClick(e.clientX - e.offsetX, e.clientY - e.offsetTop);
-    redraw();
-};
+    }
 
-canvas.onmousemove = (e) => {
-    if (paint) {
-        addClick(e.clientX - e.offsetX, e.clientY - e.offsetTop);
-        redraw();
+    updateCurrentPosition(e) {
+        this.prevPosition = this.currentPosition;
+        this.currentPosition = {x: e.layerX, y: e.layerY};
+    }
+
+    draw() {
+        this.context.beginPath();
+        this.context.moveTo(this.prevPosition.x, this.prevPosition.y);
+        this.context.lineTo(this.currentPosition.x, this.currentPosition.y);
+        this.context.stroke();
     }
 }
 
-canvas.onmouseup = () => {
-    paint = false;
-};
-
-canvas.onmouseleave = ()=>{
-    paint = false;
-}
+ new DrawCanvas();
